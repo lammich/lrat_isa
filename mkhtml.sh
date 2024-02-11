c118c9df89f48e744b06c5a56341b08ffa2fa975
Peter Lammich <plammich@gmail.com>
2024-02-11 02:55:45 +0100

    Added html browsable thy files

--- mkhtml.sh
+++ mkhtml.sh
@@ -9,8 +9,8 @@ ISABELLE_BROWSER_INFO=$($ISABELLE getenv ISABELLE_BROWSER_INFO | sed -re 's/.*=/
 rm -rf html
 mkdir -p html
 
-cp -a "$ISABELLE_BROWSER_INFO" "html/"
+cp -a "$ISABELLE_BROWSER_INFO"/* "html/"
 
 
-pandoc -V pagetitle="LRUP Check LLVM" -s index.md > html/index.html
+# pandoc -V pagetitle="LRUP Check LLVM" -s index.md > html/index.html
 
